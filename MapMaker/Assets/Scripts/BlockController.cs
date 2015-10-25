835f1ca6fb67ee931d460f3fcef5418a2adc6454
Austin Andrews <a5mercuryhu@gmail.com>
2015-10-24 19:27:17 -0500

    Add ability to add blocks to the map

--- MapMaker/Assets/Scripts/BlockController.cs
+++ MapMaker/Assets/Scripts/BlockController.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BlockController : MonoBehaviour {
 
-	public GameObject block;
+	public static bool doUpdate = true;
+	public Dictionary<string, GameObject> blocks;
+	public GameObject useBlock;
+	public GameObject blockPrefab;
 	public MapMaker map;
-	
-	public GameObject[] blocks;
 	
 	private Transform blockHolder;
 	
 	void Start() 
 	{
 		// Start by instantiating the starting block
-		block = Instantiate(block, new Vector3(0, 0.25f, 0), Quaternion.identity) as GameObject;
+		//block = InstantiateBlock();
 		
 		// Create a new array with the size of whatever the number of possible blocks on the map can be
-		blocks = new GameObject[(int)map.mapSize.x * (int)map.mapSize.y];
+		blocks = new Dictionary<string, GameObject>();
+		//blocks = new GameObject[(int)map.mapSize.x * (int)map.mapSize.y];
 		
 		// Set the parent for where the blocks are to go
 		string holderName = "WorldBlocks";
@@ -26,40 +29,78 @@ public class BlockController : MonoBehaviour {
 	
 	void Update() 
 	{
+		// Return if space is pressed
+		if ( ! doUpdate) { 
+			// Move the block out of view temporarily
+			Destroy(useBlock);
+			return;
+		}
+		
+		// Build a new block
+		if (Input.GetKeyDown(KeyCode.B)) {
+			useBlock = CreateBlock();
+		}
+		
 		// Place a block
 		if (Input.GetKey(KeyCode.Mouse0)) {
-			PlaceBlock();
+			PlaceBlock(useBlock);
 		}
 	
 		// Move the block around the map
-		MoveBlock();
+		MoveBlock(useBlock);
 		
-		// ##### DEBUG ##### \\
+		
 		if (Input.GetKeyDown (KeyCode.P)) {
-			foreach (GameObject b in blocks) {
-				if (b != null) {
-					Debug.Log (b.name);
-				}
+			foreach (string B in blocks.Keys) {
+				Debug.Log(blocks[B]);
 			}
 		}
-		// ################# \\
+	}
+	
+	public GameObject CreateBlock()
+	{
+		GameObject newBlock = InstantiateBlock(0, 0);
+		newBlock.name = "Block(" + newBlock.transform.position.x + "," + newBlock.transform.position.z + ")";
+		newBlock.transform.parent = blockHolder;
+	
+		return newBlock;
+	}
+	
+	public GameObject InstantiateBlock(float posX, float posY)
+	{
+		return Instantiate(blockPrefab, new Vector3(posX, 0.25f, posY), Quaternion.identity) as GameObject;
 	}
 	
 	// Move the block around the map with the mouse position
-	public void MoveBlock()
+	public void MoveBlock(GameObject moveBlock)
 	{
+		if ( ! moveBlock || moveBlock == null) {
+			return;
+		}
+	
 		// Cast out a ray from the camera
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		
 		// Get the object that the ray is hitting
 		RaycastHit hit;
 		if (Physics.Raycast(ray, out hit)) {			
+		
+			bool shouldMove = false;
 			
 			// If the object that is hit is a tile
 			if (hit.transform.gameObject.name == map.tilePrefab.name) {
-				
+				shouldMove = true;
+			} 
+			// If the object hit is an existing block
+			foreach (string b in blocks.Keys) {
+				if (hit.transform.gameObject.name == b) {
+					shouldMove = true;
+				}
+			}
+			
+			if (shouldMove) {
 				// Move the object to the next position
-				block.transform.position = new Vector3(
+				moveBlock.transform.position = new Vector3(
 					Mathf.Round(hit.point.x),
 					0.25f,
 					Mathf.Round(hit.point.z)
@@ -68,15 +109,16 @@ public class BlockController : MonoBehaviour {
 		}
 	}
 	
-	public void PlaceBlock()
+	public void PlaceBlock(GameObject placedBlock)
 	{
 		// Check to see if a block is located at the current position
-		if ( ! CanPlaceBlock()) {
+		// TODO: check for type when adding different kinds of blocks
+		if (blockHolder.FindChild("Block("+placedBlock.transform.position.x+","+placedBlock.transform.position.z+")")) {
 			return;
 		}
 								
 		// Create a new block in the scene
-		GameObject placedBlock = Instantiate(block, block.transform.position, Quaternion.identity) as GameObject;
+		placedBlock = Instantiate(blockPrefab, placedBlock.transform.position, Quaternion.identity) as GameObject;
 		placedBlock.name = "Block(" + placedBlock.transform.position.x + "," + placedBlock.transform.position.z + ")";
 		placedBlock.transform.parent = blockHolder;
 		
@@ -84,14 +126,19 @@ public class BlockController : MonoBehaviour {
 		SaveBlock(placedBlock);
 	}
 	
-	public bool CanPlaceBlock()
+	public bool CanPlaceBlock(GameObject checkBlock)
 	{
+		// If the block does not exist, false
+		if ( ! checkBlock || checkBlock == null) {
+			return false;
+		}
+		
 		// Loop through all the blocks in the scene and check their positions
-		if (blocks.Length > 0) {
-			for (int i = 0; i < blocks.Length; i++) {
+		if (blocks.Values.Count > 0) {
+			for (int i = 0; i < blocks.Values.Count; i++) {
 			
 				// Check the position of all the blocks
-				if (blocks[i] != null && blocks[i].transform.position == block.transform.position) {
+				if (blocks["Block("+checkBlock.transform.position.x+","+checkBlock.transform.position.z+")"] != null && blocks["Block("+checkBlock.transform.position.x+","+checkBlock.transform.position.z+")"].transform.position == checkBlock.transform.position) {
 					return false;
 				}
 			}
@@ -99,15 +146,22 @@ public class BlockController : MonoBehaviour {
 		return true;
 	}
 	
-	public void RemoveBlock()
+	public void RemoveBlock(GameObject removeBlock)
 	{
-		//
+		// Check to see if there is a block in the current mouse position
+		if ( ! CanRemoveBlock(removeBlock)) {
+			return;
+		}
+		
+		// 
 	}
 	
-	public bool CanRemoveBlock()
+	public bool CanRemoveBlock(GameObject removeBlock)
 	{
-		//
-		return true;
+		// Check to see if the block passed, is in the blocks array
+		
+				
+		return false;
 	}
 	
 	public void SaveBlocks()
@@ -117,12 +171,10 @@ public class BlockController : MonoBehaviour {
 	
 	public void SaveBlock(GameObject saveBlock)
 	{
-		// Put the block in the blocks list
-		for (int i = 0; i < blocks.Length; i++) {
-			if (blocks[i] == null) {
-				blocks[i] = saveBlock;
-				break;
-			}
+		// Put the block in the blocks dictionary
+		GameObject temp;
+		if ( ! blocks.TryGetValue("Block("+saveBlock.transform.position.x+","+saveBlock.transform.position.z+")", out temp)) {
+			blocks.Add("Block("+saveBlock.transform.position.x+","+saveBlock.transform.position.z+")", saveBlock);
 		}
 	}
 }
---
+++ MapMaker/Assets/Scripts/MapController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour {
+
+	public Camera mainCamera;
+	public MapMaker map;
+	
+
+	// Use this for initialization
+	void Start () 
+	{	
+		//
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// If SPACE is pressed, don't update the BlockController
+		if (Input.GetKey(KeyCode.Space)) {
+			BlockController.doUpdate = false;
+		} else {
+			BlockController.doUpdate = true;
+		}
+	
+		// If SPACE + MOUSE 0 is pressed, drag the camera aroud the map
+		//if (Input.GetKey(KeyCode.Space) && Input.GetKey (KeyCode.Mouse0)) {
+			DragCamera();
+		//}
+		
+		// Scroll the camera in and out
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+			Camera.main.orthographicSize--;
+		} else if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+			Camera.main.orthographicSize++;
+		}
+		Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 5, 50);
+		
+	}
+	
+	// Drag the camera around the map
+	public void DragCamera()
+	{
+		float speed = 10;
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			if (mainCamera.transform.position.x <= map.mapSize.x/2) {
+				mainCamera.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+			}
+		} 
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			if (mainCamera.transform.position.x >= -(map.mapSize.x/2)) {
+				mainCamera.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+			}
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			if (mainCamera.transform.position.z <= map.mapSize.y/2 - 20) {
+				mainCamera.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+			}
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			if (mainCamera.transform.position.z >= -(map.mapSize.y/2)) {
+				mainCamera.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+			}
+		}
+	}
+}
